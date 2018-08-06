b0ed37215522fa5340f2610528e8e127fc20bc99
Super Coder <alexchen12312@gmail.com>
2018-08-06 05:16:32 -0500

    config and update app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { Route } from 'react-router-dom';
+
+import store, { history } from './store';
+
+import Home from './screens/Home';
+
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <Route exact path="/" component={Home} />
+          </div>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+render(<App />, rootElement);
+
 registerServiceWorker();
